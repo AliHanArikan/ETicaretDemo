34a391f8c4d0fbd4e7b5a052a9704bdb97cfa377
AliHanArikan <alihanarkan4@gmail.com>
2024-08-10 16:44:48 +0200

    DiscountMikroservisi_Yazildi

--- Services/Catalog/ETicaret.Catalog/Program.cs
+++ Services/Catalog/ETicaret.Catalog/Program.cs
@@ -1,11 +1,33 @@
+using ETicaret.Catalog.Services.CategoryServices;
+using ETicaret.Catalog.Services.ProductDetailDetailServices;
+using ETicaret.Catalog.Services.ProductDetailServices;
+using ETicaret.Catalog.Services.ProductImagesServices;
+using ETicaret.Catalog.Services.ProductServices;
+using ETicaret.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ICatagoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value; 
+}); 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
